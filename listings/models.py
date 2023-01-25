5ce812eee6b4d29198c4fa0d2548b3f2c98d914b
rowan siwakoti <rowansiwakoti@gmail.com>
2023-01-25 21:06:39 +0545

    set up complete

---
+++ listings/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from datetime import datetime
+from realtors.models import Realtor
+
+# Create your models here.
+class Listing(models.Model):
+  realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING) #Realtor is another model
+  title = models.CharField(max_length=200)
+  address = models.CharField(max_length=200)
+  city = models.CharField(max_length=100)
+  state = models.CharField(max_length=100)
+  zipcode = models.CharField(max_length=20)
+  description = models.TextField(blank=True)
+  price = models.IntegerField()
+  bedrooms = models.IntegerField()
+  bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+  garage = models.IntegerField(default=0)
+  sqft = models.IntegerField()
+  lot_size = models.DecimalField(max_digits=5, decimal_places=1)
+  photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+  photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  is_published = models.BooleanField(default=True)
+  list_date = models.DateTimeField(default=datetime.now, blank=True)
+  def __str__(self): #In the Django Admin, there will be a table that displays each listing and we need to pick the main field
+                     #that needs to be display, so we choose 'title' 
+    return self.title
+
+
+
+
+
+    # image size reducers link
+    # https://medium.com/@eyakubsorkar/how-to-compress-image-while-uploading-to-the-directory-django-bdd3fcab0b23
+    # https://pythoncircle.com/post/707/how-to-compress-the-uploaded-image-before-storing-it-in-django/
+    # https://stackoverflow.com/questions/52183975/how-to-compress-the-image-before-uploading-to-s3-in-django
